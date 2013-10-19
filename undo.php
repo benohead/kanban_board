e7433aa31c19b82be2218f8ff943da607228a81f
henri <hb@amazingweb.de>
2013-10-19 13:53:40 +0200

    test

--- undo.php
+++ undo.php
@@ -2,7 +2,7 @@
 require_once("models/config.php");
 if (!securePage($_SERVER['PHP_SELF'])){
 	die();
-} 
+}
 require_once "functions.php";
 $board_id = $_POST['boardid'];
 $previousversion = getLastHistoryVersionId($board_id);
